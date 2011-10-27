5045f2198aba8879b2faeabdb87e135c003bbef6
Kristoffer Berdal <web@flexd.net>
2011-10-27 15:17:06 +0200

    I did stuff

--- src/Person.java
+++ src/Person.java
@@ -4,104 +4,98 @@
  * Jan E. Vandevjen - s180494
  * Tommy Nyrud - s180487
  * Informasjonsteknologi 1IA og
- * Dataingeniør 1AA
+ * Dataingeniï¿½r 1AA
  */
 
-//Denne klassen sørger for konstruksjon og kontroll av Person objekter
-
+//Denne klassen sï¿½rger for konstruksjon og kontroll av Person objekter
 import java.util.Date;
 import java.text.DateFormat;
 import javax.swing.JOptionPane;
 
-public class Person
-{
-	private String navn;
-	private int id;
-	private Sykkel sykkel;
-	private String merknad = "";
-	private Date startTid;
-	private static int nesteNr = 0; //Global tellevariabel for alle person objektene
-   
-	//Konstruktør som også gir Person objektet en ny id
-	public Person(String navn) {
-		this.navn = navn;
-		id = nesteNr++;	
-	}
-	
-	public int getID() {
-		return id;
-	}
+public class Person {
 
-   public Sykkel getSykkel() { 
-     return sykkel;
-   }
-   
-   //Sjekker om personen verken eier en sykkel eller har en merknad
-   public boolean godkjent()
-   {
+  private String navn;
+  private int id;
+  private Sykkel sykkel;
+  private String merknad = "";
+  private Date startTid;
+  private static int nesteNr = 0; //Global tellevariabel for alle person objektene
+
+  //Konstruktï¿½r som ogsï¿½ gir Person objektet en ny id
+  public Person(String navn) {
+    this.navn = navn;
+    id = nesteNr++;
+  }
+
+  public int getID() {
+    return id;
+  }
+
+  public Sykkel getSykkel() {
+    return sykkel;
+  }
+
+  //Sjekker om personen verken eier en sykkel eller har en merknad
+  public boolean godkjent() {
     if (sykkel == null && merknad.equals("")) {
       return true;
     }
     return false;
-   }
-   
-   //Oppretter, og legger til merknader på brukeren
-   public void setMerknad(Date t, String m) {
-     DateFormat df = DateFormat.getInstance(); 
-     merknad+= df.format(t) + " : " + m + "\n";
-     JOptionPane.showMessageDialog(null, "Følgende merknad er registert - " + merknad);
-   }
-   
-   //Knytter en bruker til en sykkel
-   public boolean leiSykkel(Sykkel s) {
-     if(godkjent()) {
+  }
+
+  //Oppretter, og legger til merknader pï¿½ brukeren
+  public void setMerknad(Date t, String m) {
+    DateFormat df = DateFormat.getInstance();
+    merknad += df.format(t) + " : " + m + "\n";
+    JOptionPane.showMessageDialog(null, "Fï¿½lgende merknad er registert - " + merknad);
+  }
+
+  //Knytter en bruker til en sykkel
+  public boolean leiSykkel(Sykkel s) {
+    if (godkjent()) {
       sykkel = s;
       startTid = new Date();
       return true;
-     }
-     return false;
-   }
-   
-   //Regner ut leietid, rundet opp til nærmeste hele time
-   public int leietid(Date sluttTid) {
-    long varighet = (sluttTid.getTime() - startTid.getTime()); 
+    }
+    return false;
+  }
+
+  //Regner ut leietid, rundet opp til nï¿½rmeste hele time
+  public int leietid(Date sluttTid) {
+    long varighet = (sluttTid.getTime() - startTid.getTime());
     int varighetTimer = (int) Math.ceil(varighet / 3600000);
-	   
+
     return varighetTimer;
-   }
-   
-   //Leverer inn sykkelen, og registerer en merknad om bruker har levert for sent.
-   public void leverInn() {
-     Date innTid = new Date();
-	   
-     if(leietid(innTid) > Sykkel.getMAXTID()) {
-		   
-      if(leietid(innTid) - Sykkel.getMAXTID() == 1 ) {
+  }
+
+  //Leverer inn sykkelen, og registerer en merknad om bruker har levert for sent.
+  public void leverInn() {
+    Date innTid = new Date();
+
+    if (leietid(innTid) > Sykkel.getMAXTID()) {
+
+      if (leietid(innTid) - Sykkel.getMAXTID() == 1) {
         setMerknad(innTid, "Sykkel ble levert " + (leietid(innTid) - Sykkel.getMAXTID()) + " time for sent");
-      }
-		   
-      else {
+      } else {
         setMerknad(innTid, "Sykkel ble levert " + (leietid(innTid) - Sykkel.getMAXTID()) + " timer for sent");
       }
-     }
-     sykkel = null;
-   }
+    }
+    sykkel = null;
+  }
 
-  
-   //Skriver ut relevant informasjon om brukeren
+  //Skriver ut relevant informasjon om brukeren
   @Override
-   public String toString() {
-	   String utskrift = navn + " ID nummer: " + id + "\n";
-	   
-	   if(sykkel != null) {
-		   utskrift += "Sykkel id: " + sykkel.getID() + "\n";
-	   }
-	   
-	   if(merknad != "") {
-		   utskrift += merknad;
-	   }
-	   
-	   return utskrift;
-   }
+  public String toString() {
+    String utskrift = navn + " ID nummer: " + id + "\n";
 
-} 
+    if (sykkel != null) {
+      utskrift += "Sykkel id: " + sykkel.getID() + "\n";
+    }
+
+    if (merknad != "") {
+      utskrift += merknad;
+    }
+
+    return utskrift;
+  }
+}
--- src/Utleiested.java
+++ src/Utleiested.java
@@ -4,162 +4,126 @@
  * Jan E. Vandevjen - s180494
  * Tommy Nyrud - s180487
  * Informasjonsteknologi 1IA og
- * Dataingeni�r 1AA 
+ * Dataingeni�r 1AA 
  */
 
+// Klasse som håndterer Utleiestedene, Vindusklassen og events.
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+public class Utleiested extends JFrame implements ActionListener {
 
-public class Utleiested extends JFrame implements ActionListener
-{
-   private Stativ stativet;
-   private JTextField  personIDFelt, sykkelIDFelt;
-   private JTextArea display;
-   private JLabel personIDlabel, sykkelIDlabel;
-   private JButton ut, inn ;
-   private PersonRegister personer;
+  private Stativ stativet;
+  private JTextField personIDFelt, sykkelIDFelt;
+  private JTextArea display;
+  private JLabel personIDlabel, sykkelIDlabel;
+  private JButton ut, inn;
+  private PersonRegister personer;
 
-   public Utleiested(String navn, int antall, PersonRegister p)
-   {
-     super("BYSYKKEL " + navn);
-     personer = p;
-     stativet = new Stativ(antall);
-     
-     /*
-     < Oppretter skjermkomponentene >
+  public Utleiested(String navn, int antall, PersonRegister p) {
+    super("BYSYKKEL " + navn);
+    personer = p;
+    stativet = new Stativ(antall);
 
-     < Knytter knappene til lytteobjektet >
+    personIDlabel = new JLabel("Person ID");
+    personIDFelt = new JTextField(4);
+    ut = new JButton("Leie");
+    sykkelIDlabel = new JLabel("Sykkel ID");
+    sykkelIDFelt = new JTextField(4);
+    inn = new JButton("Levere");
 
-     Container c = getContentPane();
-     setLayout(new FlowLayout());
+    // Lytte etter hendelser.
 
-     < Legger skjermkomponentene ut på vinduet >
-      * 
-      */
-     personIDlabel = new JLabel("Person ID");
-     personIDFelt = new JTextField(4);
-     ut = new JButton("Leie");
-     sykkelIDlabel = new JLabel("Sykkel ID");
-     sykkelIDFelt = new JTextField(4);
-     inn = new JButton("Levere");
-     
-     // Lytte etter hendelser.
-     
-     ut.addActionListener(this);
-     inn.addActionListener(this);
-     display = new JTextArea(6,15);
-     display.setEditable(false);
-     display.setLineWrap(true);
-     
-     JScrollPane content = new JScrollPane(display);
-     content.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-     Container c = getContentPane();
-    
-     setLayout(new FlowLayout());
-     
-     c.add(personIDlabel);
-     c.add(personIDFelt);
-     c.add(ut);
-     c.add(sykkelIDlabel);
-     c.add(sykkelIDFelt);
-     c.add(inn);
-     c.add(content);
-     
-     setSize(200, 200);
-     setVisible(true);
-     setResizable(false);
-     setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-   }
+    ut.addActionListener(this);
+    inn.addActionListener(this);
+    display = new JTextArea(6, 15);
+    display.setEditable(false);
+    display.setLineWrap(true);
 
-   public void leiUt()
-   {
-     /*
-     < Metoden skal leie ut en sykkel til personen med det id-nummer som er
-       skrevet inn i tekstfeltet for dette. Hvis personen er registrert og er
-       godkjent for utleie, skal det skrives ut i tekstområdet hvilken sykkel
-       vedkommende skal ta. I motsatt fall skal det skrives ut en passende
-       feilmelding. Denne skal inneholde årsaken til at personen ikke fikk leie
-       sykkelen. >
-      */
-	  boolean failed = false;
-	  int personID = -1;
-	  try {
-	   String personIDstring = personIDFelt.getText();
-	   personID = Integer.parseInt(personIDstring)-1;
-	  }
-	  catch (Exception e) {
-	    failed = true;
-	  }
-      
-     if (!failed) {
-       Person person = personer.finnPerson(personID);
-       if (person != null) {
-         // Success, vi har en person.
-         String resultat = stativet.leiUt(person);
-         display.append(resultat);
-       }
-       else {
-         // Sorry personen finnes ikke i registeret.
-         display.append("Personen finnes ikke i register\n");
-       }
-     }
-     
-   }
+    JScrollPane content = new JScrollPane(display);
+    content.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+    Container c = getContentPane();
 
-   public void leverInn()
-   {
-     /*
-     < Metoden skal registrere at en sykkel leveres inn. Hvis sykkelen er
-       registrert utleid skal innleveringen registreres på personen som har
-       leid sykkelen og det må registreres at sykkelen settes tilbake i stativet.
-       I tekstområdet skal det skrives hvor sykkelen skal plasseres. Hvis
-       sykkelens id-nummer er ukjent, skal det gis beskjed om det. >
-      * 
-      */
-	   boolean failed = false;
-	   int sykkelID = -1;
-	   try {
-		 String sykkelIDstring = sykkelIDFelt.getText(); 
-	     sykkelID = Integer.parseInt(sykkelIDstring)-1;
-	   }
-		  catch (Exception e) {
-		    failed = true;
-		  }
-     if (!failed) {
-       Person person = personer.finnSykkelBruker(sykkelID);
-       String resultat = "";
+    setLayout(new FlowLayout());
 
-       if (person != null) {
-         // Success, vi har en person.
-         resultat = stativet.leverInn(person);
-       }
-       else {
+    c.add(personIDlabel);
+    c.add(personIDFelt);
+    c.add(ut);
+    c.add(sykkelIDlabel);
+    c.add(sykkelIDFelt);
+    c.add(inn);
+    c.add(content);
+
+    setSize(200, 200);
+    setVisible(true);
+    setResizable(false);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  // Leier ut sykkelen
+  public void leiUt() {
+    boolean failed = false;
+    int personID = -1;
+    try {
+      String personIDstring = personIDFelt.getText();
+      personID = Integer.parseInt(personIDstring) - 1;
+    } catch (Exception e) {
+      failed = true;
+    }
+
+    // Ingen problemer med å parse Stringen i tekstfeltet til integer.
+    if (!failed) {
+      Person person = personer.finnPerson(personID);
+      if (person != null) {
+        // Success, vi har en person.
+        String resultat = stativet.leiUt(person);
+        display.append(resultat);
+      } else {
+        // Sorry personen finnes ikke i registeret.
+        display.append("Personen finnes ikke i register\n");
+      }
+    }
+
+  }
+
+  // Metode for å levere inn sykkel, 
+  public void leverInn() {
+
+    boolean failed = false;
+    int sykkelID = -1;
+    try {
+      String sykkelIDstring = sykkelIDFelt.getText();
+      sykkelID = Integer.parseInt(sykkelIDstring) - 1;
+    } catch (Exception e) {
+      failed = true;
+    }
+    // Ingen problemer, yay!
+    if (!failed) {
+      Person person = personer.finnSykkelBruker(sykkelID);
+      String resultat = "";
+
+      if (person != null) {
+        // Success, vi har en person.
+        resultat = stativet.leverInn(person);
+      } else {
         resultat = "Det finnes ingen person med dette ID eller så har ingen leid denne sykkelen.\n";
-       }
-         display.append(resultat);
-     }
-     else {
+      }
+      display.append(resultat);
+    } else {
       // Sorry personen finnes ikke i registeret.
       display.append("Du må faktisk skrive inn et id\n");
-     }
-   }
+    }
+  }
 
+  // Enkel @override metode for å håndtere knapper! :-D
   @Override
-   public void actionPerformed(ActionEvent e )
-   {
-     /*
-     < Metoden skal sørge for at leiUt() kalles når det klikkes på knappen
-       "Leie", og at leverInn() kalles når det klikkes på  knappen "Levere" >
-      * 
-      */
-     if (e.getSource() == ut) {
-       leiUt();
-     }
-     else if (e.getSource() == inn) {
-       leverInn();
-     }
-   }
+  public void actionPerformed(ActionEvent e) {
 
+    if (e.getSource() == ut) {
+      leiUt();
+    } else if (e.getSource() == inn) {
+      leverInn();
+    }
+  }
 }// end of class Utleiested
